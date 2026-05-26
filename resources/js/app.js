73e64635aef4b66fbe8a5aa30051cff508e9ec71
1RYouka <studywithchandra20@gmail.com>
2026-05-26 13:22:44 +0700

    use local jsqr dependency for qr scanner

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@ import './bootstrap';
 import './image-upload-compression';
 
 import Alpine from 'alpinejs';
+import jsQR from 'jsqr';
 
 window.Alpine = Alpine;
+window.jsQR = jsQR;
 
 Alpine.start();
--- resources/views/orders/scan_transaction.blade.php
+++ resources/views/orders/scan_transaction.blade.php
@@ -104,7 +104,6 @@
 @endsection
 
 @section('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.js"></script>
     <script>
         (() => {
             const resolveTemplate = @json($resolveTemplate);
